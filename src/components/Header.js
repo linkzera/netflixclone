129a5a9d0626153066953b60caad27f7c23bad16
Douglas <douglas04041996@hotmail.com>
2022-01-21 07:06:16 -0300

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
+import logo from "../logo.png";
 
-export default () => {
-    return(
-        <header>
-            <div className="header--logo">
-                <a href="/">
-                    <img src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="logo"></img>
-                </a>
-            </div>
+export default function Header() {
+  return (
+    <header>
+      <div className="header--logo">
+        <a href="/">
+          <img src={logo} alt="logo netflix"></img>
+        </a>
+      </div>
 
-            <div className="header--user">
-                <a href="/">
-                    <img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png' alt='user-logo'></img>    
-                </a>    
-                
-            </div>        
-        </header>
-    )
+      <div className="header--user">
+        <a href="/">
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+            alt="user-logo"
+          ></img>
+        </a>
+      </div>
+    </header>
+  );
 }
--- src/components/Movierow.js
+++ src/components/Movierow.js
@@ -1,21 +1,24 @@
 import React from "react";
-import './Movierow.css'
+import "./Movierow.css";
 
-export default ({title, items}) => {
-    return(
-        <div className="movieRow">
-            <h2>{title}</h2>
-            <div className="movieRow--listarea">
-                <div className="movieRow--list">
-                    {items.results.length > 0 && items.results.map((item,key) =>(
-                       <div key={key} className="movieRow--item"> <img src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`} alt={item.original_title}></img></div>
-                        
-                    ) )}
-                </div>
-
-               
-            </div>
+export default function movieRow({ title, items }) {
+  return (
+    <div className="movieRow">
+      <h2>{title}</h2>
+      <div className="movieRow--listarea">
+        <div className="movieRow--list">
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div key={key} className="movieRow--item">
+                {" "}
+                <img
+                  src={`https://image.tmdb.org/t/p/w300/${item.poster_path}`}
+                  alt={item.original_title}
+                ></img>
+              </div>
+            ))}
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
